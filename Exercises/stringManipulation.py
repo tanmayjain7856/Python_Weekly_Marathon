2b6f49d6962ac04a1c7b2f174947ebdfb3a66de9
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-04 17:23:38 +0530

    Add exercises

---
+++ Exercises/stringManipulation.py
@@ -0,0 +1,4 @@
+print('Week 1 - String Maipulation')
+print('String concatenation is done with the "+" sign.')
+print('e.g. print("Hello" + " World")')
+print('New lines can be created with a backslash and n.')
