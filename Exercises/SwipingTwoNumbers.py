82d3e94211a1850f0d903c2958c73b28e70de081
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-04 18:10:32 +0530

    Add swiping two numbers program

---
+++ Exercises/SwipingTwoNumbers.py
@@ -0,0 +1,11 @@
+a = input('a: ')
+b = input('b: ')
+print('Original Values: ')
+print('a: ' + a)
+print('b: ' + b)
+temp = a
+a = b
+b = temp
+print('Swipped Values: ')
+print('a: ' + a)
+print('b: ' + b)
