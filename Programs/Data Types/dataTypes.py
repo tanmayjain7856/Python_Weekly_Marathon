bb144fde7493b9b6e380a0763283870690c1ab81
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-05 11:44:45 +0530

    Add Data Types

---
+++ Programs/Data Types/dataTypes.py
@@ -0,0 +1,17 @@
+# Data Types
+
+# Strings
+print("Hello World")
+print('Hello'[2])  # Subscripting #Output: l
+print('123' + '345')
+
+# Integers
+print(12 + 24)  # Output: 36
+print(123_456_789)  # large number can be represented using _
+
+# Floating-Point
+print(17.49 + 107.576)
+
+# Boolean
+print(True)
+print(False)
