7e53ef16d609074465b01e256fe928044609bfce
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-12 16:25:59 +0530

    Add number manipulation and f-strings

---
+++ Programs/Number Manipulation and f-Strings/f-Strings.py
@@ -0,0 +1,9 @@
+# without f-strings
+# print("The value of pi is: " + 3.14)
+# error as only strings can be concatenated
+
+# 1st way: to convert number to string
+print('The value of pi is :' + str(3.14))
+
+# 2nd way: to use f-strings
+print(f'The value of pi is: {3.14}')
---
+++ Programs/Number Manipulation and f-Strings/numberManipulation.py
@@ -0,0 +1,10 @@
+# round(no. to be rounded, <precision (optional)>)
+print(round(20/7))
+print(round(20/7, 4))
+
+# format(no.to be formatted), {:.<no. of decimal points>f}
+pi = 3.14285714286
+print('Value of pi is: ' + '{:.3f}'.format(pi))
+
+# Floor Division (//)
+print(20//7)
