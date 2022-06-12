a72a42a1850e7d418c7cf792d07f9e304f7cbd51
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-12 13:53:04 +0530

    Add numerical operations

---
+++ Programs/Numerical Operations/numericalOperations.py
@@ -0,0 +1,21 @@
+# Addition (+)
+print(10+20)
+
+# Subtraction (-)
+print(30-15)
+
+# Multiplication (*)
+print(5*7)
+
+# Division (/)
+print(100/4)
+
+# Exponent (**)
+print(2 ** 6)
+
+# Modulo Operator (%)
+print(200 % 3)
+
+# Order of priority (L->R)
+print(3 * 3 + 3 / 3 - 3)  # 7
+print(3 * (3 + 3) / 3 - 3)  # 3
