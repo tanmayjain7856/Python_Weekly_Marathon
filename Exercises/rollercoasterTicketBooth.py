cbe434a1c95624a41077f7ea5dc3c9568b412658
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-19 11:05:33 +0530

    Add nested if-else statement

--- Exercises/rollercoasterTicketBooth.py
+++ Exercises/rollercoasterTicketBooth.py
@@ -3,5 +3,10 @@ height = input('Enter your height in cm: ')
 heightInCM = int(height)
 if heightInCM >= 120:
     print('Yay!! You can ride the rollercoaster.')
+    age = int(input('Enter your age: '))
+    if age >= 18:
+        print('Your ticket price will be $12.')
+    else:
+        print('your ticket price will be $7.')
 else:
     print("Sorry!! You can't ride the rollercoaster.")
