beaae0cdba2b59f36f855fbfa6ef64fb54fb7ded
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-19 18:30:42 +0530

    Add multiple if statements

--- Exercises/rollercoasterTicketBooth.py
+++ Exercises/rollercoasterTicketBooth.py
@@ -1,14 +1,22 @@
 print('Welcome to Rollercoaster!!!')
 height = input('Enter your height in cm: ')
+bill = 0
 heightInCM = int(height)
 if heightInCM >= 120:
     print('Yay!! You can ride the rollercoaster.')
     age = int(input('Enter your age: '))
     if age < 12:
-        print('Your ticket price will be $5.')
-    elif age <= 18:
-        print('your ticket price will be $7.')
+        bill = 5
+        print('Child tickets are $5.')
+    if age <= 18:
+        bill = 7
+        print('Youth tickets are $7.')
     else:
-        print('your ticket price will be $12.')
+        bill = 12
+        print('Adult tickets are $12.')
+    wantsPhoto = input('Do you want photos of yourself? Y or N. ')
+    if wantsPhoto == 'Y':
+        bill = bill + 3
+    print(f'your final bill is ${bill}')
 else:
     print("Sorry!! You can't ride the rollercoaster.")
