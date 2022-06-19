e13733ed7b25ad520cd1807b8361312acd3d980e
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-19 11:27:25 +0530

    Upgrade BMI Calculator using elif statements

--- Exercises/BMICalculator.py
+++ Exercises/BMICalculator.py
@@ -1,4 +1,13 @@
 height = input('Enter your height in m: ')
 weight = input('Enter your weight in kg: ')
-bmi = int(weight) / (float(height) ** 2)
-print('Your BMI is ' + str(int(bmi)))
+bmi = round(int(weight) / (float(height) ** 2), 2)
+if bmi < 18.5:
+    print(f'Your BMI is {bmi} and you are underweight.')
+elif bmi < 25:
+    print(f'Your BMI is {bmi} and you are healthy.')
+elif bmi < 30:
+    print(f'Your BMI is {bmi} and you are overweight.')
+elif bmi < 35:
+    print(f'Your BMI is {bmi} and you are obese.')
+else:
+    print(f'Your BMI is {bmi} and you are clinically obese.')
