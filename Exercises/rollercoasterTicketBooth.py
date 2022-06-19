6e32c974c93b4d6bd82c0d49a77bbe02650377dd
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-19 11:16:00 +0530

    Add elif statement

--- Exercises/rollercoasterTicketBooth.py
+++ Exercises/rollercoasterTicketBooth.py
@@ -4,9 +4,11 @@ heightInCM = int(height)
 if heightInCM >= 120:
     print('Yay!! You can ride the rollercoaster.')
     age = int(input('Enter your age: '))
-    if age >= 18:
-        print('Your ticket price will be $12.')
-    else:
+    if age < 12:
+        print('Your ticket price will be $5.')
+    elif age <= 18:
         print('your ticket price will be $7.')
+    else:
+        print('your ticket price will be $12.')
 else:
     print("Sorry!! You can't ride the rollercoaster.")
