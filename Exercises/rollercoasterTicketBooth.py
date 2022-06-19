6730e061be93b9987e0d58d3982a73cad9de9fd2
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-19 10:44:27 +0530

    Add rollercoaster ticket booth program

---
+++ Exercises/rollercoasterTicketBooth.py
@@ -0,0 +1,7 @@
+print('Welcome to Rollercoaster!!!')
+height = input('Enter your height in cm: ')
+heightInCM = int(height)
+if heightInCM >= 120:
+    print('Yay!! You can ride the rollercoaster.')
+else:
+    print("Sorry!! You can't ride the rollercoaster.")
