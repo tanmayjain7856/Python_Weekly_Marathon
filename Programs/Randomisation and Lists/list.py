4f6d4a10e7ea616ba20acac1f3d04aa3628f4e76
tanmayjain7856 <tanmayjain987@gmail.com>
2022-06-25 18:23:26 +0530

    Add lists

---
+++ Programs/Randomisation and Lists/list.py
@@ -0,0 +1,22 @@
+# Lists
+colors = ['blue', 'crimson', 'green', 'indigo', 'orange',
+          'purple', 'red', 'violet', 'white', 'yellow']
+
+# To print whole list
+print(colors)
+
+# To print a particular data in list
+print(colors[2])
+print(colors[-1])
+
+# To change an item in list
+colors[2] = 'grey'
+print(colors[2])
+
+# To add an item to list
+colors.append('zucchini')
+print(colors)
+
+# To add multiples items to list
+colors.extend(['brown', 'olive', 'maroon', 'magenta', 'cream'])
+print(colors)
