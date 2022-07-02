bb769fbd63f55e54a795e063fcc49381961d5990
tanmayjain7856 <tanmayjain987@gmail.com>
2022-07-02 18:28:04 +0530

    Add range() function

--- Programs/Loops/forLoop.py
+++ Programs/Loops/forLoop.py
@@ -2,3 +2,17 @@ fruits = ['Apple', 'Banana', 'Orange', 'Peach']
 for fruit in fruits:
     print(fruit)
     print(fruit + ' Pie')
+
+# Range function without step
+for number in range(1, 10):
+    print(number)
+
+# Range function with step
+for number in range(1, 10, 2):
+    print(number)
+
+# adding numbers from 1 to 100
+sum = 0
+for number in range(1, 101):
+    sum += number
+print(sum)
