e6adc3ac1b9594f76e8884d5f22738fe32e477ec
tanmayjain7856 <tanmayjain987@gmail.com>
2022-07-03 13:34:42 +0530

    Add functions

---
+++ Programs/Functions/function.py
@@ -0,0 +1,16 @@
+# Predefined Functions
+print('Hello')
+num_char = len('Hello')
+print(num_char)
+
+# User functions
+# Defining a function
+
+
+def my_function():
+    print('Hello')
+    print('World')
+
+
+# Calling a function
+my_function()
