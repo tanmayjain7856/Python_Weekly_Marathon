43425b309c553b608548282f58095581e119b715
tanmayjain7856 <tanmayjain987@gmail.com>
2022-07-03 14:07:29 +0530

    Add Reeborg's World to move robot in a square

---
+++ Exercises/Reeborg's World/alone.py
@@ -0,0 +1,35 @@
+# Function for turning around
+def turn_around():
+    turn_left()
+    turn_left()
+
+# Function for turning right
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# Function for moving forward
+
+
+def move_forward():
+    move()
+    move()
+
+
+# Code for moving robot in a square
+turn_left()
+move_forward()
+turn_right()
+move_forward()
+turn_right()
+move_forward()
+turn_right()
+move_forward()
+turn_around()
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
