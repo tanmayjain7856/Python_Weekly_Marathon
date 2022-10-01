a27dc8e3de74e9b2433998b9cd2d8310675a5be3
tanmayjain7856 <tanmayjain987@gmail.com>
2022-10-01 12:46:39 +0530

    Add functions with multiple inputs

--- Programs/Functions/functionWithInput.py
+++ Programs/Functions/functionWithInput.py
@@ -19,3 +19,17 @@ def greet_with_name(name):
 greet_with_name('Aman')
 greet_with_name('Sumit')
 greet_with_name('Harsha')
+
+# Function with multiple inputs
+
+
+def greet_with(name, location):
+    print(f'Hello {name}')
+    print(f'What is it like in {location}?')
+
+
+# Positional Arguments
+greet_with('Aman', 'Delhi')
+
+# Keyword Arguments
+greet_with(location='Delhi', name='Aman')
