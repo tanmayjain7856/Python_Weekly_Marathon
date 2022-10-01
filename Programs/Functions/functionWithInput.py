83fc324eed71bb9db2bfe5770beaeaee5719e3c5
tanmayjain7856 <tanmayjain987@gmail.com>
2022-10-01 12:13:43 +0530

    Add functions with input

---
+++ Programs/Functions/functionWithInput.py
@@ -0,0 +1,21 @@
+# Simple Function
+def greet():
+    print('Hello')
+    print('How are you?')
+    print('Bye')
+
+
+greet()
+
+# Function with Input
+
+
+def greet_with_name(name):
+    print(f'Hello {name}')
+    print(f'How are you {name}?')
+    print(f'Bye {name}')
+
+
+greet_with_name('Aman')
+greet_with_name('Sumit')
+greet_with_name('Harsha')
