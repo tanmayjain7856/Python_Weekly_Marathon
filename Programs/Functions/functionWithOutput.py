671e81a2b6a30240c93df533c8b6612b13b27e81
tanmayjain7856 <tanmayjain987@gmail.com>
2022-12-03 11:55:12 +0530

    Add functions with outputs

---
+++ Programs/Functions/functionWithOutput.py
@@ -0,0 +1,11 @@
+# Function with Output
+
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f'{formatted_f_name} {formatted_l_name}'
+
+
+name = format_name("TAnmAy", "jAiN")
+print(name)
